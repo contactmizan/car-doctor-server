d8a9c2ed1e2a27e9276f4009991deee9c9078d5a
contactmizan <contactmizan1206@gmail.com>
2025-03-10 07:15:58 +0600

    set bookings get and post

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
         await client.connect();
 
         const serviceCollection = client.db("carDoctor").collection("services");
+        const bookingCollection = client.db("carDoctor").collection("bookings");
 
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
@@ -39,10 +40,30 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const options = {
-                projection: { title: 1, price: 1, service_id: 1 },
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
             };
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
+        });
+
+        // booking
+
+        app.get('/bookings', async (req, res) => {
+            console.log(req.query.email);
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email };
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+
         });
 
         // Send a ping to confirm a successful connection
