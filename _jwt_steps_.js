9323713a41a9fb0d14ccd9114ad0cedf88952a2c
contactmizan <contactmizan1206@gmail.com>
2025-03-11 02:58:20 +0600

    install json web token /jwt

---
+++ _jwt_steps_.js
@@ -0,0 +1,11 @@
+/* 
+install jsonwebtoken
+jwt.sign(playload, secret, {expiresIn:})
+token client
+*/
+/**
+ * how to store token in client side
+ * 1. memory --> ok type/ not recommended
+ * 2. local storage --> ok type(XSS)
+ * 3. cookies --> http only 
+ */
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
@@ -29,7 +30,15 @@ async function run() {
 
         const serviceCollection = client.db("carDoctor").collection("services");
         const bookingCollection = client.db("carDoctor").collection("bookings");
+        // auth related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send(token);
+        })
 
+        // services related api
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
